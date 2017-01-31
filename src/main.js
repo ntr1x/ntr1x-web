ba234cf1634d81675a947329845241270fb8f915
Igor Pavlenko <ipavlenko@inbox.ru>
2017-01-31 11:27:17 +0500

    Basic home page

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
-
 import VueMaterial from 'vue-material'
+
+import AppThemes from './themes'
 import AppComponents from './components'
 
 import App from './App.vue'
@@ -11,15 +12,19 @@ import App from './App.vue'
 import Stores from './stores'
 import Pages from './pages'
 
+import 'bootstrap/dist/css/bootstrap.css'
 import 'vue-material/dist/vue-material.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.scss'
 import 'roboto-fontface/css/roboto/sass/roboto-fontface.scss'
+import './stylesheets/core.scss'
 
 Vue.use(Vuex)
 Vue.use(VueRouter)
 Vue.use(VueResource)
 Vue.use(VueMaterial)
 Vue.use(AppComponents)
+
+Vue.material.registerTheme(AppThemes)
 
 // const store = new Vuex.Store({
 //     strict: true,
